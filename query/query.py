ac7fd66a4965ca3e9733b514967143337824d1c4
Chaoran Chen <mail@chaoran-chen.de>
2024-10-11 12:36:04 -0400

    Improve query result formatting

--- query/query.py
+++ query/query.py
@@ -3,7 +3,10 @@ import requests
 import csv
 from dataclasses import dataclass
 
-loculus_host = 'https://lapis-microbioinfo-hackathon.loculus.org/salmonella'
+
+loculus_website = 'https://microbioinfo-hackathon.loculus.org'
+loculus_lapis = 'https://lapis-microbioinfo-hackathon.loculus.org/salmonella'
+
 
 @dataclass
 class SequenceEntry:
@@ -22,7 +25,7 @@ def read_hashes_from_file(file_path: str) -> set[str]:
 
 
 def download_from_loculus() -> list[SequenceEntry]:
-    query_url = loculus_host + ('/sample/details?dataFormat=tsv'
+    query_url = loculus_lapis + ('/sample/details?dataFormat=tsv'
                                 '&fields=submissionId,groupName,groupId,collectionDate,location,profileHash')
     response = requests.get(query_url)
 
@@ -48,19 +51,33 @@ def download_from_loculus() -> list[SequenceEntry]:
         return []
 
 
-def find(query: set[str], database: list[SequenceEntry], min_proportion_matched: float) -> list[SequenceEntry]:
-    matched: list[SequenceEntry] = []
+def find(query: set[str], database: list[SequenceEntry], min_proportion_matched: float) -> list[(SequenceEntry, float)]:
+    matched: list[(SequenceEntry, float)] = []
     for database_entry in database:
-        if match(query, database_entry.profile_hash, min_proportion_matched):
-            matched.append(database_entry)
+        proportion_matched = match(query, database_entry.profile_hash)
+        if proportion_matched >= min_proportion_matched:
+            matched.append((database_entry, proportion_matched))
     return matched
 
 
-def match(query: set[str], database_entry_hashes: set[str], min_proportion_matched: float) -> bool:
+def match(query: set[str], database_entry_hashes: set[str]) -> float:
     matched_hashes = query.intersection(database_entry_hashes)
     number_matched = len(matched_hashes)
-    proportion_matched = number_matched / len(query)
-    return proportion_matched >= min_proportion_matched
+    return number_matched / len(query)
+
+
+def print_matched_entries(matched: list[(SequenceEntry, float)]):
+    matched_sorted = sorted(matched, key=lambda x: x[1], reverse=True)
+    for entry, proportion_match in matched_sorted:
+        print('{} ({}, {}), matched {:.2f}%, submitted by {} ({}/group/{})'.format(
+            entry.submission_id,
+            entry.collection_date,
+            entry.location,
+            proportion_match * 100,
+            entry.group_name,
+            loculus_website,
+            entry.group_id
+        ))
 
 
 @click.command()
@@ -71,13 +88,13 @@ def match(query: set[str], database_entry_hashes: set[str], min_proportion_match
     type=float,
     help='The minimal proportion of the queried hashes that match the entry in the database')
 def main(hash_file: str, min_proportion_matched: float):
-    print(min_proportion_matched)
+    print('Querying Patinder data with a minimal matching proportion of {:.2f}%'.format(min_proportion_matched * 100))
     query = read_hashes_from_file(hash_file)
     database = download_from_loculus()
+    print('Total number of sequences in Patinder: {}'.format(len(database)))
     matched = find(query, database, min_proportion_matched)
-    print(len(database))
-    for entry in matched:
-        print(entry.submission_id)
+    print('{} sequences match the request'.format(len(matched)))
+    print_matched_entries(matched)
 
 
 if __name__ == '__main__':
